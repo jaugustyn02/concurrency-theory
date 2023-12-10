cd8e459f539f863eb6f080824e055e8efad38362
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2023-12-10 19:55:57 +0100

    dat config removed comments

--- homeworks/Concurrent_Gaussian_Eliminator/lib/Dependency_Analysis_Tool/config.py
+++ homeworks/Concurrent_Gaussian_Eliminator/lib/Dependency_Analysis_Tool/config.py
@@ -1,11 +1,3 @@
-# INPUT_DIRECTORY_PATH = "inputs/"
-# OUTPUT_DIRECTORY_PATH = "outputs/"
-# PRINT_FNF = True
-# PRINT_PLOT = True
-# EXPORT_PLOT_TO_PNG = True
-# EXPORT_GRAPH_TO_DOT = True
-# LABELS_WITH_INDICES = False
-
 class Config:
     def __init__(
         self,
@@ -20,4 +12,3 @@ class Config:
         self.raw_transactions = raw_transactions
         self.output_directory_path = output_directory_path
         self.labels_with_indices = labels_with_indices
-    
